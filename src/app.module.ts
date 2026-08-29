9852a77e5867b0411970b2533ac9fdbdbdd81d82
JOSXH <jl670999@gmail.com>
2026-08-29 13:19:50 -0600

    chore: configuracion inicial de la BD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { createObserveModule } from '@nestjs/observe';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
@@ -13,6 +16,29 @@ export const { ObserveModule, ObserveInstrument } = createObserveModule();
       appKey: 'YOUR_APP_KEY',
       appSecret: 'YOUR_APP_SECRET',
       serviceId: 'backend',
+    }),
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      // Use useFactory, useClass, or useExisting
+      // to configure the DataSourceOptions.
+      useFactory: (configService: ConfigService) => ({
+        type: 'mssql',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        options: {
+          trustServerCertificate: true,
+        },
+        entities: [],
+        synchronize: true,
+      }),
     }),
   ],
   controllers: [AppController],
